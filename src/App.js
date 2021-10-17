ba0f9a8e8932ab2f3c51e193cb66362a8a150d27
Michael Martinez <zeroeclipse@live.com>
2021-10-16 20:57:59 -0700

    Adding Header and Home components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import PortfolioContainer from './Portfolio Container/PortfolioContainer';
 
 function App() {
   return (
     <div className="App">
-     
+      <PortfolioContainer />
     </div>
   );
 }
---
+++ src/Portfolio Container/Home/Profile/Profile.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import Typical from 'react-typical'
+import "./Profile.css"
+const Profile = () => {
+    return (
+        <div className='profile-container'>
+            <div className='profile-parent'>
+                <div className='profile-details'>
+                    <div className='colz'>
+                        <div className="colz-icon">
+                            <a href='https://github.com/lonelydomino'>
+                                <i className='fa fa-github'></i>
+                            </a>
+                            <a href='#'>
+                                <i className='fa fa-google-plus-square'></i>
+                            </a>
+                            <a href='https://www.linkedin.com/in/michael-martinez-bb49a8a0/'>
+                                <i className='fa fa-linkedin'></i>
+                            </a>
+                            <a href='https://www.twitch.tv/drmilomd'>
+                                <i className='fa fa-twitch'></i>
+                            </a>
+                            <a href='#'>
+                                <i className='fa fa-twitter'></i>
+                            </a>
+                        </div>
+                    </div>
+
+                    <div className="profile-details-name">
+                        <span className="primary-test">
+                            {""}
+                            Hello, I'm <span className="highlighted-text">Michael</span>
+                        </span>
+                    </div>
+                    <div className="profile-details-role">
+                        <span className="primary-text">
+                            {" "}
+                            <h1>
+                                {" "}
+                                <Typical
+                                loop={Infinity}
+                                steps={[
+                                    "Ethusiatic Dev ", 1000,
+                                    "Full Stack Developer 💻", 1000,
+                                    "Flatiron School Graduate 🎓", 1000,
+                                    "Cheese Enthusiast 🧀", 1000,
+                                ]}/>
+
+                            </h1>
+                            <span className="profile-role-tagline">
+                                Building application front and backends.
+                            </span>
+                        </span>
+                    </div>
+                    <div className="profile-options">
+                        <button className="btn primary-btn">
+                            Hire Me
+                        </button>
+                        <a href="Resume 2021 Michael Martinez.pdf" download="Resume 2021 Michael Martinez.pdf">
+                            <button className="btn highlighted-btn">
+                                Get Resume
+                            </button>
+                        </a>
+                    </div>               
+                </div>
+                <div className="profile-picture">
+                    <div className="profile-picture-background">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
