3e47ccd4f167f121deb544e9c4f08049b15e40d1
Michael Martinez <zeroeclipse@live.com>
2021-10-17 13:08:36 -0700

    Adding carousal to Resume component.

--- src/Portfolio Container/Resume/Resume.js
+++ src/Portfolio Container/Resume/Resume.js
@@ -8,7 +8,15 @@ const Resume = (props) => {
     const [selectedBulletIndex, setSelectedBulletIndex] = useState(0)
     const [carousalOffSetStyle, setCarousalOffSetStyle] = useState({});
 
+    let fadeInScreenHandler = (screen) => {
+        if(screen.fadeScreen !== props.id) return
+        Animations.animations.fadeInScreen(props.id)
+    }
+    const fadeInSubscription = ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler)
+
+
     const ResumeHeading = (props) => {
+        return(
         <div className="resume-heading">
             <div className="resume-main-heading">
                 <div className="heading-bullet">
@@ -25,6 +33,8 @@ const Resume = (props) => {
                 </div>
             </div>
         </div>
+        )
+        
     }
 
     const resumeBullets = [
@@ -62,17 +72,135 @@ const Resume = (props) => {
             subHeading: "Technologies used: React JS, Shopify, GraphQL"
         },
     ]
+    const resumeDetails = [
+        <div className="resume-screen-container" key="education">
+            <ResumeHeading
+            heading={"University of Redlands, Redlands, CA"}
+            subHeading={"Bachelor of Arts, Biology"}
+            fromDate={"2012"}
+            toDate={"2014"}
+            />
+            <ResumeHeading
+            heading={"Flatiron School, Brooklyn, NY"}
+            subHeading={"Full Stack Web Development, Ruby on Rails and JavaScript"}
+            fromDate={"2020"}
+            toDate={"2021"}
+            />
+        </div>,
+        <div className="resume-screen-container" key="work-experience">
+            <ResumeHeading
+            heading={"Lowe's"}
+            subHeading={"Pro Sales Specialist"}
+            fromDate={"2020"}
+            toDate={"Present"}
+            />
+            <div className="experienceDescription">
+                <span className="resume-description-text">
+                    Currently working as a Pro Sales Specialist
+                </span>
+            </div>
+            <div className="experience-description">
+                <span className="resume-description-text">
+                ● Assisted customers with Lowe’s account and credit card sign-ups.
+                </span>
+                <br/>
+                <span className="resume-description-text">
+                ● Increased department sales profits by 13% in my first three months
+                </span>
+                <br/>
+                <span className="resume-description-text">
+                ● Worked with 3rd party vendors to place and plan product orders and deliveries.
+                </span>
+            </div>,
+            <div className="resume-screen-container programming-skills-container" key="programming-skills">
+                {programmingSkillDetails.map((skill, index)=>(
+                <div className="skill-parent" key={index}>
+                    <div className="heading-bullet"></div>
+                    <span>{skill.skill}</span>
+                    <div className="skill-percentage">
+                        <div style={{width: skill.ratingPercentage + "%"}} className="active-percentage">
 
-    let fadeInScreenHandler = (screen) => {
-        if(screen.fadeScreen !== props.id) return
-        Animations.animations.fadeInScreen(props.id)
+                        </div>
+                    </div>
+                </div>
+                ))}
+            </div>,
+
+            <div className="resume-screen-container" key="projects">
+                {projectDetails.map((projectDetails, index) =>(
+                    <ResumeHeading
+                    key={index}
+                    heading={projectDetails.title}
+                    subHeading={projectDetails.subHeading}
+                    description={projectDetails.description}
+                    fromDate={projectDetails.duration.fromDate}
+                    toDate={projectDetails.duration.toDate}
+                    />
+                ) )}
+            </div>,
+            <div className="resume-screen-container" key="interests">
+                <ResumeHeading 
+                heading="Gaming"
+                description="I love gaming"
+                />
+                <ResumeHeading 
+                heading="Programming"
+                description="I love Programming"
+                />
+                <ResumeHeading 
+                heading="Music"
+                description="I love Music"
+                />
+            </div>         
+        </div>
+    ]
+
+    const handleCarousal = (index) => {
+        let offsetHeight = 360
+        let newCarousalOffset = {
+            style: {transform: "translateY("+ index * offsetHeight * -1 + "px)"},
+        }
+        setCarousalOffSetStyle(newCarousalOffset)
+        setSelectedBulletIndex(index)
     }
-    const fadeInSubscription = ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler)
+
+    const getBullets = () => {
+        return resumeBullets.map((bullet, index) => (
+            <div onclick={() => handleCarousal(index)}
+            className={index === setSelectedBulletIndex ? "bullet selected-bullet" : "bullet"}
+            key={index}>
+                <img className="bullet-logo"
+                src={require (`../../assets/Resume/${bullet.logoSrc}`).default}
+                alt="oops, no logo"
+            />
+
+
+            </div>
+        ))
+    }
+
+    const getResumeScreen = () => {
+        return (
+            <div style={carousalOffSetStyle.style}
+            className="resume-details-carousal">
+                {resumeDetails.map((ResumeDetail) => ResumeDetail)}
+            </div>
+        )
+    }
 
      return (
      <div className="resume-container screen-container" id={props.id || ""}>
          <div className="resume-content">
              <ScreenHeading title={"Resume"} subHeading={"My Formal Bio Details"}/>
+             <div className="resume-card">
+                 <div className="resume-bullets">
+                     <div className="bullet-container">
+                         <div className="bullet-icons"></div>
+                         <div className="bullets">{getBullets()}</div>
+                     </div>
+                 </div>
+                 <div className="resume-bullets-details">{getResumeScreen()}</div>
+             </div>
          </div>
         
      </div>
